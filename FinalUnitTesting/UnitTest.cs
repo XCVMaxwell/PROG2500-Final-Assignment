4916825ba5a4a2de19dbf8aa136853dbeaa0727f
XCVMaxwell <maxmuir204@gmail.com>
2021-04-19 16:47:14 -0300

    Add first unit test

--- FinalUnitTesting/UnitTest.cs
+++ FinalUnitTesting/UnitTest.cs
@@ -1,6 +1,7 @@
-﻿
-using System;
+﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PokemonTcgSdk.Models;
+using FinalApp.Repos;
 
 namespace FinalUnitTesting
 {
@@ -8,9 +9,15 @@ namespace FinalUnitTesting
     public class UnitTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GetAllPokemonCards_GetsAllCards()
         {
+            var cards = PokemonRepo.GetAllPokemonCards();
+            var firstCard = cards[0];
+            var name = firstCard.Name;
 
+            Assert.IsTrue(cards.Count > 20);
+            Assert.IsNotNull(firstCard);
+            Assert.IsNotNull(name);
         }
     }
 }
